6114a56472f2db113f86dd63172d162aab96b50b
Afletch <austinj.fletcher@gmail.com>
2019-09-23 11:24:50 -0700

    remove spotify-web-api-node

--- liriBot.js
+++ liriBot.js
@@ -1,6 +1,6 @@
 // Include packages
 const Spotify = require('node-spotify-api');
-var SpotifyWebApi = require('spotify-web-api-node');
+//var SpotifyWebApi = require('spotify-web-api-node');
 const inquirer = require("inquirer");
 const axios = require("axios");
 
@@ -68,57 +68,6 @@ function returnSpotifyType() {
 }
 
 // Spotify-web-api-node
-function searchSpotify() {
-  var spotifyType = "";//returnSpotifyType();
-  var searchTerm = ""; //returnSearchTerm();  
- 
-  inquirer
-  .prompt([
-    {
-      type: "list",
-      message: "What are you going to search for?",
-      name: "userSearchType",
-      choices: ["Artist","Album","track"]
-    }
-  ])
-  .then(function(user) {
-    console.log(user.userSearchType)
-    spotifyType = user.userSearchType.toLowerCase();
-
-    inquirer
-    .prompt([
-      {
-        type: "input",
-        message: "What would you like to search for?",
-        name: "userSearchTerm"
-      }
-    ])
-    .then(function(user) {
-      console.log(user.userSearchTerm)
-      searchTerm = user.userSearchTerm.toLowerCase();
-
-      searchTerm = spotifyType + ":" + searchTerm;
-      var spotifyApi = new SpotifyWebApi({
-        // clientId: '1f9422099bf64dbb866280479ed9b1cc',
-        // clientSecret: '21e27afe3bde4d87b9b31aa76def69be',
-        clientId: 'fcecfc72172e4cd267473117a17cbd4d',
-        clientSecret: 'a6338157c9bb5ac9c71924cb2940e1a7',
-        redirectUri: 'http://www.example.com/callback'
-      });
-
-      spotifyApi.searchTracks(searchTerm)
-      .then(function(data) {
-        console.log(data.body);
-      }, function(err) {
-        console.error(err);
-      });
-
-    })
-  })
-}
-
-
-// node-spotify-api
 // function searchSpotify() {
 //   var spotifyType = "";//returnSpotifyType();
 //   var searchTerm = ""; //returnSearchTerm();  
@@ -148,20 +97,71 @@ function searchSpotify() {
 //       console.log(user.userSearchTerm)
 //       searchTerm = user.userSearchTerm.toLowerCase();
 
-//       var spotify = new Spotify({
-//         id: "1f9422099bf64dbb866280479ed9b1cc",
-//         secret: "21e27afe3bde4d87b9b31aa76def69be"
+//       searchTerm = spotifyType + ":" + searchTerm;
+//       var spotifyApi = new SpotifyWebApi({
+//         // clientId: '1f9422099bf64dbb866280479ed9b1cc',
+//         // clientSecret: '21e27afe3bde4d87b9b31aa76def69be',
+//         clientId: 'fcecfc72172e4cd267473117a17cbd4d',
+//         clientSecret: 'a6338157c9bb5ac9c71924cb2940e1a7',
+//         redirectUri: 'http://www.example.com/callback'
 //       });
-//       spotify.search({ type: spotifyType, query: searchTerm, limit: 5 }, function(err, data) {
-//         if (err) {
-//           return console.log('Error occurred: ' + err);
-//         }
-//       console.log(JSON.stringify(data.body)); 
+
+//       spotifyApi.searchTracks(searchTerm)
+//       .then(function(data) {
+//         console.log(data.body);
+//       }, function(err) {
+//         console.error(err);
 //       });
 
 //     })
 //   })
 // }
+
+
+// node-spotify-api
+function searchSpotify() {
+  var spotifyType = "";//returnSpotifyType();
+  var searchTerm = ""; //returnSearchTerm();  
+ 
+  inquirer
+  .prompt([
+    {
+      type: "list",
+      message: "What are you going to search for?",
+      name: "userSearchType",
+      choices: ["Artist","Album","track"]
+    }
+  ])
+  .then(function(user) {
+    console.log(user.userSearchType)
+    spotifyType = user.userSearchType.toLowerCase();
+
+    inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What would you like to search for?",
+        name: "userSearchTerm"
+      }
+    ])
+    .then(function(user) {
+      console.log(user.userSearchTerm)
+      searchTerm = user.userSearchTerm.toLowerCase();
+
+      var spotify = new Spotify({
+        id: "1f9422099bf64dbb866280479ed9b1cc",
+        secret: "21e27afe3bde4d87b9b31aa76def69be"
+      });
+      spotify.search({ type: spotifyType, query: searchTerm, limit: 5 }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+      console.log(data); 
+      });
+
+    })
+  })
+}
  
 
 
