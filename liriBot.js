d68f54532ef27bd2487317980ccd26cb8310daf6
Afletch <austinj.fletcher@gmail.com>
2019-09-24 23:35:59 -0700

    spotify api sorta works

--- liriBot.js
+++ liriBot.js
@@ -1,6 +1,5 @@
 // Include packages
 const Spotify = require('node-spotify-api');
-//var SpotifyWebApi = require('spotify-web-api-node');
 const inquirer = require("inquirer");
 const axios = require("axios");
 
@@ -13,19 +12,20 @@ function main_menu() {
       type: "list",
       message: "Main Menu",
       name: "mainMenuChoice",
-      choices: ["Music Search","Movie Search","Live band search"]
+      choices: ["Song Search","Movie Search","Live band search"]
     }
   ])
   .then(function(user) {
     switch (user.mainMenuChoice){
-      case "Music Search":
+      case "Song Search":
         // TODO: Search Spotify api
         console.log("Spotify Api");
         searchSpotify();
         break;
       case "Movie Search":
           // TODO: Search OMDB api
-          console.log("OMDB Api");        
+          console.log("OMDB Api");
+          searchOmdbApi()        
           break;
       case "Live band search":
           // TODO: Search bandsInTown api
@@ -50,119 +50,90 @@ function returnSearchTerm() {
     return user.userSearchTerm;
   })
 }
-// Get spotify type
-function returnSpotifyType() {
-  inquirer
-  .prompt([
-    {
-      type: "list",
-      message: "What are you going to search for?",
-      name: "userSearchType",
-      choices: ["Artist","Album","track"]
-    }
-  ])
-  .then(function(user) {
-    console.log(user.userSearchType)
-    return user.userSearchType;
-  })
-}
-
-// Spotify-web-api-node
-// function searchSpotify() {
-//   var spotifyType = "";//returnSpotifyType();
-//   var searchTerm = ""; //returnSearchTerm();  
- 
-//   inquirer
-//   .prompt([
-//     {
-//       type: "list",
-//       message: "What are you going to search for?",
-//       name: "userSearchType",
-//       choices: ["Artist","Album","track"]
-//     }
-//   ])
-//   .then(function(user) {
-//     console.log(user.userSearchType)
-//     spotifyType = user.userSearchType.toLowerCase();
-
-//     inquirer
-//     .prompt([
-//       {
-//         type: "input",
-//         message: "What would you like to search for?",
-//         name: "userSearchTerm"
-//       }
-//     ])
-//     .then(function(user) {
-//       console.log(user.userSearchTerm)
-//       searchTerm = user.userSearchTerm.toLowerCase();
-
-//       searchTerm = spotifyType + ":" + searchTerm;
-//       var spotifyApi = new SpotifyWebApi({
-//         // clientId: '1f9422099bf64dbb866280479ed9b1cc',
-//         // clientSecret: '21e27afe3bde4d87b9b31aa76def69be',
-//         clientId: 'fcecfc72172e4cd267473117a17cbd4d',
-//         clientSecret: 'a6338157c9bb5ac9c71924cb2940e1a7',
-//         redirectUri: 'http://www.example.com/callback'
-//       });
-
-//       spotifyApi.searchTracks(searchTerm)
-//       .then(function(data) {
-//         console.log(data.body);
-//       }, function(err) {
-//         console.error(err);
-//       });
-
-//     })
-//   })
-// }
-
 
 // node-spotify-api
 function searchSpotify() {
-  var spotifyType = "";//returnSpotifyType();
-  var searchTerm = ""; //returnSearchTerm();  
- 
+  var searchTerm = "Mr. Nobody"; //returnSearchTerm();  
   inquirer
   .prompt([
     {
-      type: "list",
-      message: "What are you going to search for?",
-      name: "userSearchType",
-      choices: ["Artist","Album","track"]
+      type: "input",
+      message: "What song would you like to search for?",
+      name: "userSearchTerm"
     }
   ])
   .then(function(user) {
-    console.log(user.userSearchType)
-    spotifyType = user.userSearchType.toLowerCase();
+    console.log(user.userSearchTerm)
+    if (user.userSearchTerm != ""){
+      searchTerm = user.userSearchTerm.toLowerCase();}
 
-    inquirer
-    .prompt([
-      {
-        type: "input",
-        message: "What would you like to search for?",
-        name: "userSearchTerm"
+    var spotify = new Spotify({
+      id: "1f9422099bf64dbb866280479ed9b1cc",
+      secret: "21e27afe3bde4d87b9b31aa76def69be"
+    });
+    spotify.search({ type: 'track', query: searchTerm, limit: 1 }, function(err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
       }
-    ])
-    .then(function(user) {
-      console.log(user.userSearchTerm)
-      searchTerm = user.userSearchTerm.toLowerCase();
-
-      var spotify = new Spotify({
-        id: "1f9422099bf64dbb866280479ed9b1cc",
-        secret: "21e27afe3bde4d87b9b31aa76def69be"
-      });
-      spotify.search({ type: spotifyType, query: searchTerm, limit: 5 }, function(err, data) {
-        if (err) {
-          return console.log('Error occurred: ' + err);
-        }
-      console.log(data); 
-      });
-
-    })
+    console.log(JSON.stringify(data.tracks.items[0].album.artists,null,2)); 
+    });
   })
 }
  
+function searchOmdbApi() {
+  // var searchTerm = returnSearchTerm();
+
+  inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What Movie would you like to search for?",
+      name: "userSearchTerm"
+    }
+  ])
+  .then(function(user) {
+    console.log(user.userSearchTerm)
+   let searchTerm = user.userSearchTerm;
+  
+
+
+
+  var queryUrl = "http://www.omdbapi.com/?t=" + searchTerm + "&y=&plot=short&apikey=trilogy";
+
+  axios.get(queryUrl)
+  .then(function(respose) {
+    console.log(respose.data.Title);
+    console.log(respose.data.Year);
+    console.log(respose.data.Rated);
+    console.log(respose.data.Released);
+    console.log(respose.data.Runtime);
+    console.log(respose.data.Genre);
+    console.log(respose.data.Director);
+    console.log(respose.data.Plot);
+    
+  })
+  .catch(function(error) {
+    if(error.respose){
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log("---------------Data---------------");
+      console.log(error.response.data);
+      console.log("---------------Status---------------");
+      console.log(error.response.status);
+      console.log("---------------Status---------------");
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an object that comes back with details pertaining to the error that occurred.
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log("Error", error.message);
+    }
+    console.log(error.config);    
+  })
+})
+}
 
 
 main_menu();
